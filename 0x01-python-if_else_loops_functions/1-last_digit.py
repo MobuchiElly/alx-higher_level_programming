d5fd26d336508abdd7620b820b8296dc985e3708
MobuchiElly <ugwueleazer@gmail.com>
2023-08-08 15:25:41 -0700

    Determining las digit of random numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+l_digt = abs(number) % 10
+if l_digt > 5:
+    print(f'last digit of {number} is {l_digt} and is greater than 5')
+if l_digt == 0:
+    print(f'last digit of {number} is {l_digt} and is 0')
+if l_digt < 6 and not l_digt == 0:
+    print(f'last digit of {number} is -{l_digt} and is less than 6 and not 0')

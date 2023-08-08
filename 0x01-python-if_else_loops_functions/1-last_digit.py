a0d0912aba578dc2a6690356e6f09c954da92502
MobuchiElly <ugwueleazer@gmail.com>
2023-08-08 15:31:52 -0700

    Python script that determines the last digit of any random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 l_digt = abs(number) % 10
 if l_digt > 5:
-    print(f'last digit of {number} is {l_digt} and is greater than 5')
+    print(f'Last digit of {number} is {l_digt} and is greater than 5')
 if l_digt == 0:
-    print(f'last digit of {number} is {l_digt} and is 0')
+    print(f'Last digit of {number} is {l_digt} and is 0')
 if l_digt < 6 and not l_digt == 0:
-    print(f'last digit of {number} is -{l_digt} and is less than 6 and not 0')
+    print(f'Last digit of {number} is -{l_digt} and is less than 6 and not 0')

cdcb16dd5bd7ac8fec0cee9dc82bd8f70c31ee17
MobuchiElly <ugwueleazer@gmail.com>
2024-04-01 01:36:14 +0100

    printing using string formatting and applying conditional checks on the separator

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 for i in range(0, 100):
     sep = ', ' if i < 99 else '\n'
     if i < 10:
-        print(f"0{i}", end=sep)
+        print("0{0}".format(i), end=sep)
     else:
-        print(f"{i}", end=sep)
+        print("{0}".format(i), end=sep)

9f1d297ab7750ecdfc733197756991efb783780d
MobuchiElly <ugwueleazer@gmail.com>
2024-04-02 13:57:59 +0100

    print alpha in rev and capitalise

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a')-1, -1):
-    print(chr(i-32 if i % 2 != 0 else i), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
+    print("{0}".format(chr(i - 32) if (ord('z') - i) % 2 != 0 else chr(i)), end="")
+

571a01b319b43c7b4a8f532429accff18c7b768d
MobuchiElly <ugwueleazer@gmail.com>
2024-09-19 13:52:59 +0100

    update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,3 +1,6 @@
-#!/usr/bin/ython3
+#!/usr/bin/python3
 def number_keys(a_dictionary):
-    return len(a_dictionary)
+    count = 0
+    for keys in a_dictionary:
+        count = count + 1
+    return count
